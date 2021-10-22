6ad8e1b95e03e67793320a630eaa21c8f50e1b11
Cyrus <cywong@vtc.edu.hk>
2021-10-22 02:47:22 -0400

    Complete amazon textract state machine by looping as temp fix for stepfunction bug - SNSNotification to SnsNotification parameter for textract.

--- src/construct/amazon-textract-multi-pages-documents-state-machine-construct.ts
+++ src/construct/amazon-textract-multi-pages-documents-state-machine-construct.ts
@@ -2,8 +2,9 @@ import { PolicyStatement, Role, ServicePrincipal } from '@aws-cdk/aws-iam';
 import { Bucket } from '@aws-cdk/aws-s3';
 import * as sns from '@aws-cdk/aws-sns';
 import * as sfn from '@aws-cdk/aws-stepfunctions';
-import { StateMachine } from '@aws-cdk/aws-stepfunctions';
+import { Choice, Pass, StateMachine, Wait } from '@aws-cdk/aws-stepfunctions';
 import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
+import { WaitTime } from '@aws-cdk/aws-stepfunctions/lib/states/wait';
 import { Construct, Duration } from '@aws-cdk/core';
 
 
@@ -38,7 +39,7 @@ export class AmazonTextractMultiPagesDocumentsStateMachineConstruct extends Cons
       service: 'textract',
       action: 'startDocumentAnalysis',
       parameters: {
-        ClientRequestToken: sfn.JsonPath.stringAt('$$.Execution.Id'),
+        ClientRequestToken: sfn.JsonPath.stringAt('$$.Execution.Name'),
         DocumentLocation: {
           S3Object: {
             Bucket: this.pdfSourceBucket.bucketName,
@@ -46,7 +47,7 @@ export class AmazonTextractMultiPagesDocumentsStateMachineConstruct extends Cons
           },
         },
         FeatureTypes: ['FORMS'],
-        JobTag: sfn.JsonPath.stringAt('$$.Execution.Id'),
+        JobTag: sfn.JsonPath.stringAt('$$.Execution.Name'),
         // NotificationChannel: {
         //   RoleArn: textractExecutionRole.roleArn,
         //   SnsTopicArn: amazonTextractJobCompleteTopic.topicArn,
@@ -58,9 +59,57 @@ export class AmazonTextractMultiPagesDocumentsStateMachineConstruct extends Cons
       },
       iamResources: ['*'],
       iamAction: 'textract:StartDocumentAnalysis',
+      resultSelector: {
+        JobId: sfn.JsonPath.stringAt('$.JobId'),
+      },
+      resultPath: '$.textract',
     });
 
-    const definition = runAmazonTextract;
+    const getDocumentAnalysis = new tasks.CallAwsService(this, 'GetDocumentAnalysis', {
+      service: 'textract',
+      action: 'getDocumentAnalysis',
+      parameters: {
+        JobId: sfn.JsonPath.stringAt('$.textract.JobId'),
+      },
+      iamResources: ['*'],
+      iamAction: 'textract:GetDocumentAnalysis',
+      resultSelector: {
+        JobStatus: sfn.JsonPath.stringAt('$.JobStatus'),
+      },
+      resultPath: '$.status',
+    });
+
+    const jobFailed = new sfn.Fail(this, 'Job Failed', {
+      cause: 'Amazon Textract Job Failed',
+      error: 'DescribeJob returned FAILED',
+    });
+    const jobFinish = new sfn.Pass(this, 'Job Finish', {
+      comment: 'AWS Textract Job Finish',
+      parameters: {
+        key: sfn.JsonPath.stringAt('$.key'),
+        JobId: sfn.JsonPath.stringAt('$.textract.JobId'),
+        textractPrefix: sfn.JsonPath.stringAt('States.Format(\'{}/{}\', $.key, $.textract.JobId)'),
+      },
+    });
+
+    const handleDataLimitExceeded = new Pass(this, 'DataLimitExceeded').next(jobFinish);
+    getDocumentAnalysis.addCatch(handleDataLimitExceeded, {
+      errors: ['States.DataLimitExceeded'],
+      resultPath: '$.error-info',
+    });
+
+
+    const wait = new Wait(this, 'Wait 1 minute', {
+      comment: 'Wait 1 minute\'',
+      time: WaitTime.duration(Duration.minutes(1)),
+    });
+
+    const choice = new Choice(this, 'Check Job Status')
+      .when(sfn.Condition.stringEquals('$.status.JobStatus', 'FAILED'), jobFailed)
+      .when(sfn.Condition.stringEquals('$.status.JobStatus', 'SUCCEEDED'), jobFinish)
+      .otherwise(wait);
+
+    const definition = runAmazonTextract.next(wait).next(getDocumentAnalysis).next(choice);
 
     this.stateMachine = new sfn.StateMachine(this, 'StateMachine', {
       definition,
