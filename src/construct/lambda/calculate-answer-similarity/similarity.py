7613f2d44d47036831627499a3d46abfc0e3efcc
EC2 Default User <ec2-user@ip-172-31-87-122.ec2.internal>
2021-11-10 01:34:49 +0000

    completed similarity lambda.

--- src/construct/lambda/calculate-answer-similarity/similarity.py
+++ src/construct/lambda/calculate-answer-similarity/similarity.py
@@ -1,11 +1,11 @@
 from sentence_transformers import SentenceTransformer, util
 
-# model = SentenceTransformer('all-MiniLM-L6-v2',cache_folder="/tmp/")
 model = SentenceTransformer('./model',cache_folder="/tmp/")
 
-
 def handler(event, context):
-    sentences = event["studentAnswer"] + event["standardAnswer"]
+    sentences = event["studentAnswer"]
+    sentences.insert(0,event["standardAnswer"])
+    print(sentences)
 
     # Compute embeddings
     embeddings = model.encode(sentences, convert_to_tensor=True)
@@ -14,10 +14,9 @@ def handler(event, context):
     cosine_scores = util.pytorch_cos_sim(embeddings, embeddings)
 
     # Find the pairs with the highest cosine similarity scores
-    pairs = []
+    pairs = {}
     for j in range(1, len(cosine_scores)):
-        pairs.append({'text': sentences[j], 'score': cosine_scores[0][j]})
+        pairs[sentences[j]] = float(cosine_scores[0][j])
 
-    # Sort scores in decreasing order
-    pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
     return pairs
+
